000524a24cdc3c3e21fe72cfd309266291bac496
Baima Songa <baimasonga@gmail.com>
2026-05-11 12:29:28 +0000

    more modification

--- obj/Debug/net8.0/SalesApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/SalesApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SalesApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+55f1ca1a780e8203d938b9b9dc2e1af0c6addc6a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d7f179b3ed05a53ffddd2c6b49ac51c5a59d17a1")]
 [assembly: System.Reflection.AssemblyProductAttribute("SalesApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SalesApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
